e12a17fb3db1b94c88e8bf8eb9a736a0d0261fe1
Alex Pattison <alexlpattison@gmail.com>
2016-09-16 11:27:59 -0700

    Fix syntax error

--- client/journalapp/app/Main.js
+++ client/journalapp/app/Main.js
@@ -296,6 +296,7 @@ export default class Main extends Component {
         <WhiteListScene
           navigator={ navigator } />
       )
+    }
   }
 
   // Note that all the Components are enclosed in the navigator. It sets the initial route to Main,
